18bed2e0ab6fcb5980b8fd964d91257a5be466cd
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2025-11-23 17:40:11 +0530

    Q16

---
+++ module_2/Day_5 github/javascriptAss1/Arrow Function/Q16.js
@@ -0,0 +1,9 @@
+const isEven=(n)=>n%2 === 0;
+console.log(isEven(10));
+console.log(isEven(7));
+let marks = 30;
+const result=marks>=35?"pass":"fail"
+console.log(result);
+const greet=(name)=>name?`hello,${name}`:"Guest";
+console.log(greet("raju"));
+console.log(greet());
