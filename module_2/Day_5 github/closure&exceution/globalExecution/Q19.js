d2bb86da30fca996a0a7dca4124b8fbf476fee82
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2025-11-26 21:27:22 +0530

    Q19

---
+++ module_2/Day_5 github/closure&exceution/globalExecution/Q19.js
@@ -0,0 +1,11 @@
+var age=20;
+function displayAge(){
+  console.log("Age is:",age); 
+}
+function changeAge(){
+    age="25";
+    console.log("updatedAge is:",age);
+}
+displayAge();
+changeAge();
+displayAge();
